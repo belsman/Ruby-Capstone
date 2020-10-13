ca48a33d061d0e77a23322434a25c5661f088ab8
Bello S Babakolo <fleetbeekay@gmail.com>
2020-10-13 21:12:12 +0100

    Remove url hard-coded link

--- lib/spider.rb
+++ lib/spider.rb
@@ -12,7 +12,7 @@ class CovidWebSpider
 
   def make_connection
     begin
-      @doc = Nokogiri::HTML(URI.open('https://www.worldometers.info/coronavirus/'))
+      @doc = Nokogiri::HTML(URI.open(url))
       @connected = true
     rescue StandardError
       @connected = false
